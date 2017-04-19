616c8f35a22498b42613d44e8fc795fd6607891c
Salmon-Bard <Salmon-Bard@users.noreply.github.com>
2017-04-19 04:13:50 +0300

    Update NativeAudio.java

--- src/android/NativeAudio.java
+++ src/android/NativeAudio.java
@@ -85,8 +85,8 @@ public class NativeAudio extends CordovaPlugin implements AudioManager.OnAudioFo
         String fullPath = assetPath;
 
         Context ctx = cordova.getActivity().getApplicationContext();
-        AssetManager am = context.getAssets();
-        AssetFileDescriptor afd = am.openFd(fullPath);
+        AssetManager am = ctx.getResources().getAssets();
+        AssetFileDescriptor afd = setDataSource(context, Uri.parse(fullPath);
 
         NativeAudioAsset asset = new NativeAudioAsset(
             afd, voices, (float)volume);
